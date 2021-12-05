f989f1f4266f85d97422f2aa5dc18781549326d7
Petar Kostić <petar@kostic.dev>
2021-12-05 16:20:35 +0100

    Solved Day05, removed unused imports, applied hlint

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -4,16 +4,9 @@ module Days.Day05 (runDay) where
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +14,42 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parseLine `sepBy` endOfLine
+
+parseLine :: Parser Line
+parseLine = do firstX <- decimal 
+               char ','
+               firstY <- decimal
+               string " -> "
+               sndX <- decimal
+               char ','
+               sndY <- decimal
+               return ((firstX, firstY), (sndX, sndY))
 
 ------------ TYPES ------------
-type Input = Void
 
-type OutputA = Void
+type Point = (Int, Int)
+type Line = (Point, Point)
+type Input = [Line]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA ls = intersectingPoints (foldMap getPoints (filter isStraight ls))
+         where isStraight ((a, b), (c, d)) = a == c || b == d
 
+intersectingPoints :: [Point] -> Int
+intersectingPoints ls = Map.size (Map.filter (>1) (counts ls))
+
+counts :: [Point] -> Map Point Int
+counts = foldr (flip (Map.insertWith (+)) 1) Map.empty
+
+getPoints :: Line -> [Point]
+getPoints ((a, b), (c, d)) = zip (range a c) (range b d)
+                        where range start end = [start, start + signum (end - start) .. end]
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB ls = intersectingPoints (foldMap getPoints ls)
