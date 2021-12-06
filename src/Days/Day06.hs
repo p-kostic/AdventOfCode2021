81f4cbe919f91169945fbd139ebb7dc6d39154db
Petar Kostić <petar@kostic.dev>
2021-12-06 11:10:26 +0100

    Solved Day06, removed unused imports, applied hlint

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,16 +1,6 @@
 module Days.Day06 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
@@ -21,19 +11,31 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` char ','
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = length . simulate 80
+
+simulate :: Int -> [Int] -> [Int]
+simulate 0 xs = xs
+simulate n xs = simulate (n-1) (step xs)
+
+step :: [Int] -> [Int]
+step []     = []
+step (0:xs) = 6 : 8 : step xs
+step (x:xs) = (x-1) : step xs
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map (\x -> countFish x !! 256)
+
+countFish :: Int -> [Int]
+countFish n = replicate (n + 1) 1 ++ zipWith (+) (countFish 6) (countFish 8)
