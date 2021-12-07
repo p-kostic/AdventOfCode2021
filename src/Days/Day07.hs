52618f7562e0463b0ac69b2e54a69898c6e75c8c
Petar Kostić <petar@kostic.dev>
2021-12-07 10:55:43 +0100

    Solved Day07, removed unused imports, applied hlint

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,19 +1,9 @@
 module Days.Day07 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.List
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +11,41 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` char ','
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA xs = sum $ getFuelCost (median xs) <$> xs  
+
+-- Median function was borrowed and slightly modified to remove 'Fractional a =>' from https://stackoverflow.com/a/51092619/4516065
+medianFromSorted :: Num a => Integral a => [a] -> a
+medianFromSorted []      = 0
+medianFromSorted [a]     = a
+medianFromSorted [a,b]   = floor (fromIntegral (a + b) / 2)
+medianFromSorted [a,b,c] = b
+medianFromSorted (a:xs)  = medianFromSorted (init xs)
+
+median :: Ord a => Num a => Integral a => [a] -> a
+median = medianFromSorted . sort
+
+getFuelCost :: Int -> Int -> Int 
+getFuelCost x y = abs (x - y)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB xs = sum (getFuelCost' (mean xs) <$> xs)
+
+getFuelCost' :: Int -> Int -> Int
+getFuelCost' x y = d * (d+1) `div` 2
+                 where d = abs (x - y)
+
+mean :: Num a => Integral a => [a] -> a
+mean xs = floor (fromIntegral (sum xs) / fromIntegral (length xs))
+
