173ac897d465eaf1de6feb308015dabf15c95e7d
Petar Kostić <petar@kostic.dev>
2021-12-09 13:31:53 +0100

    Solved Day09, removed unused imports, applied hlint

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,19 +1,11 @@
 module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
-
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text hiding (take)
+import Data.Char (digitToInt)
+import Data.Ord
+import Data.List (sortOn)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +13,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (map digitToInt <$> many1 digit) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
+-- NOTE for A and B: Didn't have time to make this any more efficient or readable.
+--                   Additionally, this was awful to debug..
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA xs = sum (map (\x -> 1 + getPos xs x) (lowPoint (0, 0) xs))
+
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors (x,y) = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
+
+lowPoint :: (Int, Int) -> [[Int]] -> [(Int, Int)]
+lowPoint (x,y) xs | x < length (head xs) = if isLowPoint (x,y) xs then (x,y) : lowPoint (x+1, y) xs else lowPoint (x+1, y) xs
+                  | otherwise            = if y < length xs       then lowPoint(0, y+1) xs          else []
+
+isLowPoint :: (Int, Int) -> [[Int]] -> Bool
+isLowPoint (x,y) xs = all ((> getPos xs (x,y)) . getPos xs) (neighbors (x,y))
+
+getPos :: [[Int]] -> (Int, Int) -> Int
+getPos xs (x,y) | x >= 0 && x < (length . head) xs && y >= 0 && y < length xs = xs !! y !! x
+                | otherwise                                                   = 9
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB xs = partB' (map (findBasin xs []) (lowPoint (0, 0) xs))
+         where partB' = product . take 3 . sortOn Data.Ord.Down . map (sum . map length)
+
+findBasin :: [[Int]] -> [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
+findBasin xs s (x,y) | getPos xs (x,y) < 9 && notElem (x,y) s = findBasin xs d' d
+                     | otherwise                              = s
+                     where [a,b,c,d] = neighbors (x,y)
+                           b'        = findBasin xs ((x,y):s) a
+                           c'        = findBasin xs b'        b
+                           d'        = findBasin xs c'        c
