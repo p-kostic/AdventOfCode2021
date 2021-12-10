4534a985d47d745f8eca990983e3f5690c37c1c8
Petar Kostić <petar@kostic.dev>
2021-12-10 13:50:39 +0100

    Solved Day10, removed unused imports, applied hlint

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,19 +1,11 @@
 module Days.Day10 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.List (sort)
 import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +13,49 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 (choice [char '(', char '{', char '[', char '<', char '>', char ']', char '}', char ')']) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [String]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA xs = sum (map (getScore . traverseLine []) xs)
+
+traverseLine :: String -> String -> Either Char String
+traverseLine xs []         = Right xs
+traverseLine xs ('(':ys)   = traverseLine (')':xs) ys
+traverseLine xs ('[':ys)   = traverseLine (']':xs) ys
+traverseLine xs ('{':ys)   = traverseLine ('}':xs) ys
+traverseLine xs ('<':ys)   = traverseLine ('>':xs) ys
+traverseLine (x:xs) (y:ys) | x == y    = traverseLine xs ys
+                           | otherwise = Left y
+
+getScore :: Either Char String -> Int
+getScore (Left ')') = 3
+getScore (Left ']') = 57
+getScore (Left '}') = 1197
+getScore (Left '>') = 25137
+getScore _          = 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB xs = getMiddle (mapMaybe (getScore' 0 . traverseLine []) xs)
+
+getMiddle :: [Int] -> Int
+getMiddle xs = sort xs !! max 0 (length xs `div` 2)
+
+getScore' :: Int -> Either Char String -> Maybe Int
+getScore' _ (Left _)       = Nothing
+getScore' x (Right [])     = Just x
+getScore' x (Right (y:ys)) = getScore' (5 * x + incScore y) (Right ys)
+
+incScore :: Char -> Int
+incScore ')' = 1
+incScore ']' = 2
+incScore '}' = 3
+incScore '>' = 4
